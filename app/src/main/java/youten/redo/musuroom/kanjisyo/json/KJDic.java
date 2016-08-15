cb2918e974f182b30fad15186fa639863337aabe
youten <youtenredo@gmail.com>
2016-08-15 18:30:32 +0900

    Add Warspite, Aquila, I-26, Minaduki, and Shinkai-Seikans.

--- app/src/main/java/youten/redo/musuroom/kanjisyo/json/KJDic.java
+++ app/src/main/java/youten/redo/musuroom/kanjisyo/json/KJDic.java
@@ -16,50 +16,50 @@ import youten.redo.musuroom.kanjisyo.R;
  * 艦じしょ Dictionary
  */
 public class KJDic {
-	public List<String> groups = new ArrayList<String>();
+    public List<String> groups = new ArrayList<String>();
 
-	public List<List<KJItem>> dic = new ArrayList<List<KJItem>>();
+    public List<List<KJItem>> dic = new ArrayList<List<KJItem>>();
 
-	public List<String> search(String keyword) {
-		List<String> ret = new ArrayList<String>();
-		if (keyword == null) {
-			return ret;
-		}
-		for (List<KJItem> group : dic) {
-			for (KJItem item : group) {
-				if (item.yomi.length > 0) {
-					for(String yomi1 : item.yomi) {
-						if (yomi1.indexOf(keyword) != -1) {
-							ret.add(item.kanji);
-						}
-					}
-				}
-			}
-		}
+    public List<String> search(String keyword) {
+        List<String> ret = new ArrayList<String>();
+        if (keyword == null) {
+            return ret;
+        }
+        for (List<KJItem> group : dic) {
+            for (KJItem item : group) {
+                if (item.yomi.length > 0) {
+                    for (String yomi1 : item.yomi) {
+                        if (yomi1.indexOf(keyword) != -1) {
+                            ret.add(item.kanji);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
 
-		return ret;
-	}
+        return ret;
+    }
 
-	/**
-	 * load dic.txt(json)
-	 * 
-	 * @param resources
-	 *            Context#getResources();
-	 * @return 艦じしょ
-	 */
-	public static final KJDic load(Resources resources) {
-		if (resources == null) {
-			return new KJDic(); // blank
-		}
-		InputStream is = resources.openRawResource(R.raw.dic);
-		Gson gson = new Gson();
-		KJDic ret = new KJDic();
-		try {
-			ret = (KJDic) gson.fromJson(new InputStreamReader(is, "UTF-8"),
-					KJDic.class);
-		} catch (UnsupportedEncodingException e) {
-			// ignore
-		}
-		return ret;
-	}
+    /**
+     * load dic.txt(json)
+     *
+     * @param resources Context#getResources();
+     * @return 艦じしょ
+     */
+    public static final KJDic load(Resources resources) {
+        if (resources == null) {
+            return new KJDic(); // blank
+        }
+        InputStream is = resources.openRawResource(R.raw.dic);
+        Gson gson = new Gson();
+        KJDic ret = new KJDic();
+        try {
+            ret = (KJDic) gson.fromJson(new InputStreamReader(is, "UTF-8"),
+                    KJDic.class);
+        } catch (UnsupportedEncodingException e) {
+            // ignore
+        }
+        return ret;
+    }
 }
